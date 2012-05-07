f97bedba9ef745869c45a835436baeb7b4881c6d
Kent Yoder <key@linux.vnet.ibm.com>
2012-05-07 15:21:46 -0500

    cryptroot-ask-tpm.sh: update to use new getcapability call
    This is an effort to weed out NV indexes that won't work before calling
    nv_readvalue so that the TPM dictionary attack lock doesn't engage.
    
    Signed-off-by: Kent Yoder <key@linux.vnet.ibm.com>

--- 50plymouth-tpm/cryptroot-ask-tpm.sh
+++ 50plymouth-tpm/cryptroot-ask-tpm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# package reqs: tpm-tools, trousers
+# package reqs: od, getcapability, nv_readvalue
 #
 # Author: Kent Yoder <yoder1@us.ibm.com>
 #
@@ -8,20 +8,34 @@ CRYPTSETUP=/sbin/cryptsetup
 MOUNT=/bin/mount
 UMOUNT=/bin/umount
 TPM_NVREAD=/usr/bin/nv_readvalue
-NV_INDEX_LIMIT=8
+GETCAP=/usr/bin/getcapability
 PCRS=8
 DEVICE=${1}
 NAME=${2}
 
 #set -x
 
+VIABLE_INDEXES=""
+
+#
+# An index is viable if its composite hash matches current PCR state, or if
+#it doesn't require PCR state at all
+#
+ALL_INDEXES=$($GETCAP -cap 0xd | awk -F "= " '$1 ~ /Index/ {print $2 }' | awk -F "." '{ print $1 }')
+for i in $ALL_INDEXES; do
+	MATCH=$(./getcapability -cap 0x11 -scap $i | awk -F ": " '$1 ~ /Matches/ { print $2 }')
+	if test -n "$MATCH" && test "$MATCH" = "No"; then
+		continue
+	fi
+
+	VIABLE_INDEXES="$VIABLE_INDEXES $i"
+done
+
 # plymouth feeds in this password for us
 if [ ! -n "${NVPASS}" ]; then
 	read -p "Enter your TPM NVRAM password: " NVPASS
 	echo
 fi
-
-NVINDEX=1
 
 TMPFS_MNT=/tmp/cryptroot-mnt
 if [ ! -d ${TMPFS_MNT} ]; then
@@ -39,29 +53,26 @@ if [ $? -ne 0 ]; then
 fi
 
 TMPFILE=${TMPFS_MNT}/data.tmp
+SUCCESS=0
 
-while [ ${NVINDEX} -lt ${NV_INDEX_LIMIT} ]; do
+for NVINDEX in ${VIABLE_INDEXES}; do
 	$TPM_NVREAD -ix ${NVINDEX} -sz 33 -pwdd ${NVPASS} \
 		-of ${TMPFILE} >/dev/null 2>&1
 	RC=$?
 	if [ ${RC} -eq 1 ]; then
 		#/bin/plymouth --text="TPM NV index ${NVINDEX}: Bad password."
 		echo "TPM NV index ${NVINDEX}: Bad password."
-		NVINDEX=$(( $NVINDEX + 1 ))
 		continue
 	elif [ ${RC} -eq 24 ]; then
 		#/bin/plymouth --text="TPM NV index ${NVINDEX}: PCR mismatch."
 		echo "TPM NV index ${NVINDEX}: PCR mismatch."
-		NVINDEX=$(( $NVINDEX + 1 ))
 		continue
 	elif [ ${RC} -eq 2 ]; then
 		#/bin/plymouth --text="TPM NV index ${NVINDEX}: Invalid NVRAM Index."
 		echo "TPM NV index ${NVINDEX}: Invalid NVRAM Index."
-		NVINDEX=$(( $NVINDEX + 1 ))
 		continue
 	elif [ ${RC} -ne 0 ]; then
 		echo "TPM NV index ${NVINDEX}: Unknown error (${RC})"
-		NVINDEX=$(( $NVINDEX + 1 ))
 		continue
 	fi
 
@@ -71,15 +82,15 @@ while [ ${NVINDEX} -lt ${NV_INDEX_LIMIT} ]; do
 	if [ ${RC} -ne 0 ]; then
 		#/bin/plymouth --text="TPM NV index ${NVINDEX}: wrong version"
 		echo "TPM NV index ${NVINDEX}: wrong version (${RC})"
-		NVINDEX=$(( $NVINDEX + 1 ))
 		continue
 	fi
 
+	SUCCESS=1
 	break
 done
 
 # NVRAM cannot be accessed. Fall back to LUKS passphrase
-if [ ${NVINDEX} -eq ${NV_INDEX_LIMIT} ]; then
+if [ ${SUCCESS} -eq 0 ]; then
 	echo "Unable to unlock an NVRAM area."
 	#/bin/plymouth --text "Unable to unlock an NVRAM area."
 	#/bin/plymouth ask-for-password \
@@ -89,6 +100,7 @@ if [ ${NVINDEX} -eq ${NV_INDEX_LIMIT} ]; then
 	exit -1
 fi
 
+echo "Using data read from NV index $NVINDEX"
 # copy out all but the version byte, zeroize, delete
 dd if=${TMPFS_MNT}/data.tmp of=${TMPFS_MNT}/data bs=1c skip=1 count=32 >/dev/null 2>&1
 dd if=/dev/zero of=${TMPFS_MNT}/data.tmp bs=33 count=1 >/dev/null 2>&1
